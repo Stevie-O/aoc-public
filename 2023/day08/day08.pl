74cf099be0e1bdfeab38b42a66ebc5b6b599d005
Stephen Oberholtzer <stevie@qrpff.net>
2023-12-08 16:43:57 -0500

    First working solution for 2023 Day 8
    The Allez Cuisine! rule for this day is _Use a foreign language_.
    
    I don't think it gets any more foreign than this.

---
+++ 2023/day08/day08.pl
@@ -0,0 +1,48 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Lingua::Romana::Perligata; # muahhahaha
+
+meis graphibus.
+meis itero.
+positio meo nullum da.
+
+itero parametro perlegementum da.
+itero morde.
+
+huic parametro perlegementum da.
+
+petere
+sic
+	meo symbolo iterum tum positium tum I excerpementum da.
+	positio postincresce.
+	meo extremo. extremo positium tum metamentum iterum praestantiam da.
+	nisi extremum fac sic nullum positio da cis
+	R inquementum tum symbolum aequalitas redde
+cis
+
+dum perlegementum parametro fac
+sic
+
+	meo notario hoc tum nullum tum III excerpementum da.
+	meo sinistero hoc tum VII tum III excerpementum da.
+	meo dextero hoc tum XII tum III excerpementum da.
+	
+	meis optionis sinisterum tum dexterum da.
+	
+	notario graphuum ad optionis da.
+
+cis
+
+huic AAA inquementum da.
+meo mensuro nullum da.
+
+dum ZZZ inquementum tum hoc non aequalitas fac
+sic
+	mensuro postincresce.
+	meo vertero petementum da.
+	meis optis. optis hoc graphuum arcessementa da.
+	huic da verterum optorum.
+cis
+
+mensurum tum novumversum egresso scribe.
